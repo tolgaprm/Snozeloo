2820098fae6d46bd2a6cce9631012977c1e64b97
Tolga Pirim <tolga.pirim@nextlua.com>
2025-11-04 14:18:51 +0300

    add splash screen and update app logo

--- app/src/main/java/com/prmto/snozeloo/core/MainActivity.kt
+++ app/src/main/java/com/prmto/snozeloo/core/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.prmto.snozeloo.presentation.navigation.NavigationRoot
 import com.prmto.snozeloo.presentation.theme.SnozelooTheme
@@ -14,6 +15,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
+        actionBar?.hide()
         setContent {
             SnozelooTheme {
                 val navController = rememberNavController()
